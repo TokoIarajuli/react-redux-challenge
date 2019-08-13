9e1c628778fbb2a8d70162705805976c14444c2d
TornikeIarajuli <tornikeiarajuli@gmail.com>
2019-08-13 04:44:42 +0400

    email error when autofilled needed to fix it later

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
-import AllBooks from "./components/AllBooks/AllBooks";
+import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import Wrapper from "./components/Wrapper";
+import Register from "./components/Register/Register";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Switch>
-          <Route exact path="/" component={AllBooks} />
+          <Route exact path="/" component={Register} />
           <Route path="/addbooks" component={Wrapper} />
         </Switch>
       </div>
---
+++ src/components/Register/Register.js
@@ -0,0 +1,136 @@
+import React from "react";
+import "./Register.scss";
+
+//God bless google
+const validEmailRegex = RegExp(
+  /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+);
+const validateForm = errors => {
+  let valid = true;
+  Object.values(errors).forEach(val => val.length > 0 && (valid = false));
+  return valid;
+};
+
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: null,
+      email: null,
+      lastname: null,
+      password: null,
+      errors: {
+        name: "",
+        lastname: "",
+        email: "",
+        password: ""
+      }
+    };
+  }
+
+  handleChange = e => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    let errors = this.state.errors;
+
+    switch (name) {
+      case "name":
+        errors.name =
+          value.length < 2 ? "*Name must be at least 2 characters long!" : "";
+        break;
+      case "lastname":
+        errors.lastname =
+          value.length < 2
+            ? "*Lastname must be at least 2 characters long!"
+            : "";
+      case "email":
+        errors.email = validEmailRegex.test(value)
+          ? ""
+          : "*Email is not valid!";
+        break;
+      case "password":
+        errors.password =
+          value.length < 8
+            ? "*Password must be at least 8 characters long!"
+            : "";
+        break;
+      default:
+        break;
+    }
+
+    this.setState({ errors, [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    if (validateForm(this.state.errors)) {
+      this.props.history.push("/addbooks");
+    } else {
+      alert("Invalid Form");
+    }
+  };
+
+  render() {
+    const { errors } = this.state;
+    return (
+      <div className="sign_up_form">
+        <div className="sign_up_form_box">
+          <h2>Register</h2>
+          <form onSubmit={this.handleSubmit} noValidate>
+            <label htmlFor="name">Full Name</label>
+            <input
+              type="text"
+              name="name"
+              onChange={this.handleChange}
+              noValidate
+            />
+            {errors.name.length > 0 && <span>{errors.name}</span>}
+            <label htmlFor="lastname">Lastname</label>
+            <input
+              type="text"
+              name="lastname"
+              onChange={this.handleChange}
+              noValidate
+            />
+            {errors.lastname.length > 0 && <span>{errors.lastname}</span>}
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              onChange={this.handleChange}
+              noValidate
+            />
+            {errors.email.length > 0 && <span>{errors.email}</span>}
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              onChange={this.handleChange}
+              noValidate
+            />
+            {errors.password.length > 0 && <span>{errors.password}</span>}
+            <label>Sex</label>
+            <div>
+              <input type="checkbox" id="male" />
+              <label htmlFor="male">Male</label>
+              <input type="checkbox" id="female" />
+              <label htmlFor="female">Female</label>
+              <input type="checkbox" id="helicopter" />
+              <label htmlFor="helicopter">Helicopter</label>
+            </div>
+            <label htmlFor="about">About me</label>
+            <textarea
+              rows="4"
+              cols="20"
+              maxLength="100"
+              placeholder="Max 100 characters"
+            />
+            <button>Create</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Register;
